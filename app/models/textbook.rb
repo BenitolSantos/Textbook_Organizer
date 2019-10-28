3781dbe016512960481246d3d4388faf86eb79e6
github username <github email address>
2019-10-28 03:11:05 -0400

    add subject, textbooks, and current pages view folders

--- app/models/textbook.rb
+++ app/models/textbook.rb
@@ -1,7 +1,7 @@
 class Textbook < ActiveRecord::Base
-  belongs_to :user
   belongs_to :subject
+  has_many :current_pages
+  has_many :users, through: :current_pages
   
   validates :title, presence: true
-  
 end
