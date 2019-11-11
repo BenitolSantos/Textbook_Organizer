b79be855ce160784e7af202809a1aa7f91152042
github username <github email address>
2019-11-11 11:39:55 -0500

    add find user to edit update and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,12 +32,15 @@ class UsersController < ApplicationController
     end
 
     def edit
+        @user = User.find(params[:id])
     end
 
     def update 
+        @user = User.find(params[:id])
     end
 
     def destroy
+        @user = User.find(params[:id])
     end
 
     private
