2482bcbcd5c406fe9f5d41f194376a2610a86036
github username <github email address>
2019-11-11 11:06:08 -0500

    add proper @textbook and private textbook_params method

--- app/controllers/textbooks_controller.rb
+++ app/controllers/textbooks_controller.rb
@@ -1,23 +1,30 @@
 class TextbooksController < ApplicationController
     def new   
+        @textbook = Textbook.new
     end
 
     def create 
         @textbook = Textbook.create(textbook_params)
         flash[:message] = @textbook.take_ride
-        redirect_to user_path(@ride.user)
+        redirect_to user_path(@textbook.user)
     end
 
     def edit 
+        @textbook = Textbook.find(params[:id])
     end
 
     def update
+        @textbook = Textbook.find(params[:id])
+    end
+
+    def show 
+        @textbook = Textbook.find(params[:id])
     end
 
 
     private 
 
-        def ride_params
+        def textbook_params
          params.require(:ride).permit(:user_id, :attraction_id)
         end
 end
