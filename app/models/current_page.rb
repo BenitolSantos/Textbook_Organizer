dbd439cf93697f841ba9351a005b3a35b217f8e9
github username <github email address>
2019-12-04 14:49:29 -0500

    add proper belongs_to relationships to current_page model and missing has_many relationships to user model

--- app/models/current_page.rb
+++ app/models/current_page.rb
@@ -1,7 +1,6 @@
 class Current_Page < ApplicationRecord
-   has_many :textbooks
-   has_many :users, through: :textbooks
-
+   belongs_to :textbook
+   belongs_to :user
    validates :name, presence: true
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    has_many :book
+    has_many :textbooks
     has_many :subjects, through: :textbooks
+    has_many :current_pages
     has_many :current_pages, through: :textbooks
     has_secure_password #always remember this. and bcrypt. It make the passwords secure
     validates :user_name, presence: true
