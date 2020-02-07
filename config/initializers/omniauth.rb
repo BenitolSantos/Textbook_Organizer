cb8b3f5e58c6d050e58dd8984eda00c04b7bf1cb
benitolsantos <github email address>
2020-02-07 15:04:09 -0500

    fix facebook auth problems

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+    provider :facebook, Rails.application.credentials.facebook[:facebook_key], Rails.application.credentials.facebook[:facebook_secret]
 end
+#dustin approved way^
+
+#EDITOR=nano rails credentials:edit 
+#typing in terminal this makes this encrypted and also makes you able to use the key.
+#exit with ctl x y then enter
+#remember to use the master.key in config file!
+#don't push with master.key remember to move to desktop after each use.
+
+
+The master key is now needed when using the app to hide the key and secret
