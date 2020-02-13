c1fe7c62ed3882c3f731cad39caff56708759d1b
benitolsantos <github email address>
2020-02-13 15:33:44 -0500

    merge fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     has_many :user_subjects
     has_many :subjects, through: :user_subjects
     has_many :textbooks, through: :subjects
-    has_secure_password #always remember this. and bcrypt. It make the passwords secure
+    has_secure_password #always  remember this. and bcrypt. It make the passwords secure
     validates :user_name, presence: true
  end
  
